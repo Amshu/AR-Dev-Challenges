3ea27e2a6302ac4aaddecba448b824025137c9a8
Amshu <amshumaanrakshith@gmail.com>
2019-03-21 00:05:30 +1100

    Added Unity with Week 1 and Week 3

---
+++ AR Dev Challenges/Assets/_Scripts/Week 1/TVMaterial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVMaterial : MonoBehaviour
+{
+
+    public Material linkedMaterial;
+    private WebCamTexture camTexture;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camTexture = new WebCamTexture();
+        linkedMaterial.mainTexture = camTexture;
+        camTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
