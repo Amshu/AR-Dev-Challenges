0a449d0ae63293647f77ec6265a7680d1bbbf028
Amshu <amshumaanrakshith@gmail.com>
2019-04-16 00:47:31 +1000

    Week 1 Complete

--- AR Dev Challenges/Assets/_Scripts/Week 1/TVMaterial.cs
+++ AR Dev Challenges/Assets/_Scripts/Week 1/TVMaterial.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TVMaterial : MonoBehaviour
 {
 
     public Material linkedMaterial;
-    private WebCamTexture camTexture;
+    WebCamTexture camTexture;
 
+    public Text outputText;
+    int currentCamera = 0;
 
+    void showCameras()
+    {
+        outputText.text = "";
+        foreach(WebCamDevice d in WebCamTexture.devices)
+        {
+            outputText.text += d.name + (d.name == camTexture.deviceName ? "*" : "") + "\n";
+        }
+    }
+
+    public void nextCamera()
+    {
+        currentCamera = (currentCamera + 1) % WebCamTexture.devices.Length;
+
+        // Change camera only works if the camera is stopped
+        camTexture.Stop();
+        camTexture.deviceName = WebCamTexture.devices[currentCamera].name;
+        camTexture.Play();
+        showCameras();
+    }
 
     // Start is called before the first frame update
     void Awake()
